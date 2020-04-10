71b277ad581db74610344e367380c812729a8cb1
Oddenboen <57357627+Oddenboen@users.noreply.github.com>
2020-04-10 18:47:52 +0200

    Update ting.js

--- ting.js
+++ ting.js
@@ -1,29 +1,43 @@
 var tid = 0;
 var start = 5;
 var afspil = false;
+var lyd;
+var sluk;
+var snoozeowo;
 
-
-
-let mySound;
 function preload(){
-    soundFormats('mp3', 'ogg');
-    mySound = loadSound('alarm.mp3');
+    lyd = loadSound('alarm.mp3');
 } 
 
 function setup(){
+    createCanvas(200,200)
+    sluk = createButton("stop");
+    sluk.mousePressed(togglePlaying);
+    snoozeowo = createButton("Snoozeowo");
+    snoozeowo.mousePressed(uwu);
+}
 
+function uwu(){
+    lyd.stop();
+    lyd = lyd + //VI SLUTTER HER FRA, DE SEJE ARBJEDER VIDRE HERFRA ALTSÅ JONAS OG EMIL. IKKE RASMUS OG JENNER, DOG ER JENNER OGSÅ LIDT SEJ, BARE EN SMULE.
+}
+
+function togglePlaying(){
+    lyd.stop();
 }
 
 function draw(){
-if (tid==5){
+/*
+if (tid>=start){
     afspil = true;
-}
+    return afspil;
+}*/
 
-if (afspil = true) {
-    mySound.setVolume(3.0);
-    mySound.play();
+if (tid>=start) {
+    lyd.setVolume(0.1);
+    lyd.play();
+    start = start + 1000000;
     //mySound.loop();
-    afspil = false;
 }
 
 }
